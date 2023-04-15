d28b516c418f8612ea9b6b78aa6d666e9a584742
Jonathan Perry <jonathanperry@sequencemedia.net>
2023-04-15 22:29:44 +0100

    Bytes

--- src/index.mjs
+++ src/index.mjs
@@ -6,14 +6,14 @@ export function hashKey (key) {
   return crypto.createHash('sha256').update(key).digest('base64').substring(0, 32)
 }
 
-export function encrypt (buffer, key, algorithm = 'aes-256-ctr') {
-  const iv = crypto.randomBytes(16)
+export function encrypt (buffer, key, bytes = 16, algorithm = 'aes-256-ctr') {
+  const iv = crypto.randomBytes(bytes)
   const cipher = crypto.createCipheriv(algorithm, hashKey(key), iv)
   return Buffer.concat([iv, cipher.update(buffer), cipher.final()])
 }
 
-export function decrypt (buffer, key, algorithm = 'aes-256-ctr') {
-  const iv = buffer.slice(0, 16)
+export function decrypt (buffer, key, bytes = 16, algorithm = 'aes-256-ctr') {
+  const iv = buffer.slice(0, bytes)
   const decipher = crypto.createDecipheriv(algorithm, hashKey(key), iv)
-  return Buffer.concat([decipher.update(buffer.slice(16)), decipher.final()])
+  return Buffer.concat([decipher.update(buffer.slice(bytes)), decipher.final()])
 }
