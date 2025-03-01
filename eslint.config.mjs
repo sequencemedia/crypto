4b1fc6e4355da9a35b56deedfb4db23cc99cf4da
Jonathan Perry <jonathanperry@sequencemedia.net>
2025-03-01 11:20:56 +0000

    ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,13 @@
 import globals from 'globals'
+import jsdoc from 'eslint-plugin-jsdoc'
 import standard from '@sequencemedia/eslint-config-standard/merge'
 import typescript from '@sequencemedia/eslint-config-typescript/merge'
 
 export default [
+  jsdoc.configs['flat/recommended'],
   ...standard({
     files: [
-      '**/*.{mjs,cjs}'
+      '**/*.{mjs,cjs,mts,cts}'
     ],
     languageOptions: {
       globals: {
